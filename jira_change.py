ab6a9c3d56607fc93146912f5794230c953a414d
Yashu Liang <155456843+yashuliangcelonis@users.noreply.github.com>
2024-03-22 18:43:09 +0000

    change jira status

--- jira_change.py
+++ jira_change.py
@@ -1,14 +1,27 @@
 from jira import JIRA
+import json
 
 
-jira = JIRA(server='https://celonis.atlassian.net/', basic_auth=('y.liang@celonis.com', ""))
 
-issues = jira.search_issues('development[pullrequests].all > 0 AND development[pullrequests].open = 0 AND "Release Team[Select List (multiple choices)]" = "Core Mining Engine - PQL" AND summary ~ "data-model-converter" and status = "Release Approved"')
+jira = JIRA(server='https://celonis.atlassian.net/', basic_auth=('y.liang@celonis.com', "<your jira api token>"))
+
+issues = jira.search_issues('development[pullrequests].all > 0 AND development[pullrequests].open = 0 AND "Release Team[Select List (multiple choices)]" = "Core Mining Engine - PQL" AND summary ~ "compute" and status = "Release Approved"')
 
 for issue in issues:
+    print("-------------------------")
     print(f"Issue Key: {issue.key}")
     print(f"Summary: {issue.fields.summary}")
     print(f"Status: {issue.fields.status.name}")
+    
+    # check if there is a merged pr
+    json_str = issue.fields.customfield_10000.split("json=")[1][:-1]
+    devSummaryJson = json.loads(json_str)
+    dev_field_dic = devSummaryJson["cachedValue"]["summary"]
+    
+    pr_status = dev_field_dic['pullrequest']['overall']['state']
+    print(pr_status)
+    if pr_status == 'MERGED':
+        jira.transition_issue(issue, transition='Release to production')
 
 
-print("ok")
+
